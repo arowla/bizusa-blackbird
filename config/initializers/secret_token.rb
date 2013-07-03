af2e2f44871dc2b446884a5f698e53f4c50382f2
Alison Rowland <alisonrowland@fastmail.fm>
2013-07-03 16:11:52 -0400

    Bare Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BizusaBlackbird::Application.config.secret_key_base = '76689af31465af2d7eb1778879065974f242f4f6e8350785c9f5f956e3843693492006210f509215c299232337cad5d65fe5dcab119e4aaddec46b7eb8d16176'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BizusaBlackbird::Application.config.session_store :cookie_store, key: '_bizusa-blackbird_session'
