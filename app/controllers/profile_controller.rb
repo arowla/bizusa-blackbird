20395e18cebd081ea655416771f817880f0ba955
Alison Rowland <alisonrowland@fastmail.fm>
2013-07-08 11:46:32 -0400

    Adding application and profile controller and a first stab at a layout template.

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,5 @@
+class ProfileController < ApplicationController
+  def index
+    @title = 'Blackbird'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BizusaBlackbird::Application.routes.draw do
+  root "profile#index"
+  get "profile/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
